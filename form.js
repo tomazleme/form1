02d47a0bc3fb5e14bdd76340044220b049b24e5f
tomazleme <67647378+tomazleme@users.noreply.github.com>
2023-04-17 17:46:19 -0300

    Update form.js

--- form.js
+++ form.js
@@ -19,16 +19,38 @@ const STEP_5N = '5N';
 // Seletores dos botões
 const step1NextBtn = document.querySelector('[data-current-step="1"]');
 const step2NextBtn = document.querySelector('[data-current-step="2"]');
+const step2BackBtn = document.querySelector('[data-back-from="2"]');
+
 const step3SNextBtn = document.querySelector('[data-current-step="3S"]');
+const step3SBackBtn = document.querySelector('[data-back-from="3S"]');
+
 const step4SNextBtn = document.querySelector('[data-current-step="4S"]');
+const step4SBackBtn = document.querySelector('[data-back-from="4S"]');
+
 const step5SNextBtn = document.querySelector('[data-current-step="5S"]');
+const step5SBackBtn = document.querySelector('[data-back-from="5S"]');
+
 const step6SNextBtn = document.querySelector('[data-current-step="6S"]');
+const step6SBackBtn = document.querySelector('[data-back-from="6S"]');
+
 const step7SNextBtn = document.querySelector('[data-current-step="7S"]');
+const step7SBackBtn = document.querySelector('[data-back-from="7S"]');
+
 const step8SENextBtn = document.querySelector('[data-current-step="8SE"]');
+const step8SEBackBtn = document.querySelector('[data-back-from="8SE"]');
+
 const step8SSNextBtn = document.querySelector('[data-current-step="8SS"]');
+const step8SSBackBtn = document.querySelector('[data-back-from="8SS"]');
+
 const step3NNextBtn = document.querySelector('[data-current-step="3N"]');
+const step3NBackBtn = document.querySelector('[data-back-from="3N"]');
+
 const step4NNextBtn = document.querySelector('[data-current-step="4N"]');
+const step4NBackBtn = document.querySelector('[data-back-from="4N"]');
+
 const step5NNextBtn = document.querySelector('[data-current-step="5N"]');
+const step5NBackBtn = document.querySelector('[data-back-from="5N"]');
+
 
 // ... Adicione variáveis para todos os botões das etapas seguintes
 const step2BackBtn = document.querySelector('[data-current-step="2-back"]');
